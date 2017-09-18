68ff0836b7225e45e8121ec922e675589da8df72
SubhodI <subhodpoojary@gmail.com>
2017-09-18 19:44:52 +0530

    Document contract code cover

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
 var Document = artifacts.require("./Document.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, Document);
   deployer.deploy(Document);
 };
--- test/TestDocument.sol
+++ test/TestDocument.sol
@@ -11,33 +11,53 @@ contract TestDocument {
 
     document.addDocument("name", "pReferenceURL1", "pReferenceURL2");
 
-    bytes32[4] memory expected = document.getDocument("name");
+    bytes32[4] memory uploaded = document.getDocument("name");
 
     bytes32 expectedName="name";
     bytes32 expectedURL1="pReferenceURL1";
     bytes32 expectedURL2="pReferenceURL2";
 
-    Assert.equal(expected[0], expectedName, "Name should be same");
-    Assert.equal(expected[1], expectedURL1, "Reference URL1 should be same");
-    Assert.equal(expected[2], expectedURL2, "Reference URL2 should be same");
+    Assert.equal(uploaded[0], expectedName, "Name should be same");
+    Assert.equal(uploaded[1], expectedURL1, "Reference URL1 should be same");
+    Assert.equal(uploaded[2], expectedURL2, "Reference URL2 should be same");
   }
 
-  function testUpdateDocument() {
+   function testUpdateDocument() {
+    Document document = Document(DeployedAddresses.Document());
+
+    document.updateDocument("name", "IPFS-link1", "IPFS-link2");
+
+   bytes32[4] memory uploaded = document.getDocument("name");
+
+   bytes32 expectedName="name";
+   bytes32 expectedURL1="IPFS-link1";
+   bytes32 expectedURL2="IPFS-link2";
+
+
+    Assert.equal(uploaded[0], expectedName, "Name should be same");
+    Assert.equal(uploaded[1], expectedURL1, "Reference URL1 should be IPFS-link1");
+    Assert.equal(uploaded[2], expectedURL2, "Reference URL2 should be IPFS-link2");
+  }
+
+  function testDeleteDocument() {
     Document document = Document(DeployedAddresses.Document());
 
     document.deleteDocument("name");
 
-    bytes32[4] memory expected = document.getDocument("name");
+    bytes32[4] memory uploaded = document.getDocument("name");
 
     bytes32 expectedName="";
     bytes32 expectedURL1="";
     bytes32 expectedURL2="";
 
 
-    Assert.equal(expected[0], expectedName, "Name should be empty");
-    Assert.equal(expected[1], expectedURL1, "Reference URL1 should be empty");
-    Assert.equal(expected[2], expectedURL2, "Reference URL2 should be empty");
+    Assert.equal(uploaded[0], expectedName, "Name should be empty");
+    Assert.equal(uploaded[1], expectedURL1, "Reference URL1 should be empty");
+    Assert.equal(uploaded[2], expectedURL2, "Reference URL2 should be empty");
   }
+
+ 
+ 
  
   
 }
